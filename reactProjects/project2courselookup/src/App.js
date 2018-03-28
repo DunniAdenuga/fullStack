7fe0a3bd9809cf32085ba877c0de19456c62027d
DunniAdenuga <ia005@bucknell.edu>
2018-03-28 00:13:02 -0400

    initial release

--- reactProjects/project2courselookup/src/App.js
+++ reactProjects/project2courselookup/src/App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Display from './Display';
-import { Container } from 'reactstrap';
+import HomePageTabs from './HomePageTabs';
+//import { Container } from 'reactstrap';
 
 class App extends Component {
   render() {
-    /*<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React!</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>*/
     return (
-      <Container fluid={true}>
-      <Display />
-      </Container>
+      <HomePageTabs />
     );
   }
 }
